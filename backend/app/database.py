abc6f13d3a1c9035feaf7201ff283fe0db39dea3
MINJI <always0524@naver.com>
2026-07-25 17:26:19 +0900

    feat. Docker 인프라 구축 - 백엔드/프론트엔드 Dockerfile 작성 - docker-compose로 통합 실행 환경 구성 - SQLite 데이터 영구 저장을 위한 볼륨 설정

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,9 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./finops.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./finops.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
